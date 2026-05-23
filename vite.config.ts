bb711919651fca6c7248e88541a33be93b397a4e
christina kimario <christinakimario8@gmail.com>
2026-05-23 13:40:44 +0300

    adding the backend url to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
     proxy: {
       '/api': {
-        target: 'http://localhost:8787',
+        target: 'https://anko-shop-api.christinakimario8.workers.dev/api',
         changeOrigin: true,
       },
     },
